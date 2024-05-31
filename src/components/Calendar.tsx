105005202d21f96d51a8d6fb33636e175cb05626
towmie <andre00toz@gmail.com>
2024-05-31 19:13:45 +0300

    feat: add month toggler

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,12 +1,15 @@
 import {
+  addMonths,
   eachDayOfInterval,
   endOfMonth,
   endOfWeek,
   startOfMonth,
   startOfWeek,
+  subMonths,
 } from "date-fns";
 import { useMemo, useState } from "react";
 import CalendarDay from "./CalendarDay";
+import { formatDate } from "../utils.js/utils";
 
 export default function Calendar() {
   const [selectedMonth, setSelectedMont] = useState(new Date());
@@ -23,12 +26,26 @@ export default function Calendar() {
   return (
     <div className="calendar">
       <div className="header">
-        <button className="btn">Today</button>
+        <button className="btn" onClick={() => setSelectedMont(new Date())}>
+          Today
+        </button>
         <div>
-          <button className="month-change-btn">&lt;</button>
-          <button className="month-change-btn">&gt;</button>
+          <button
+            className="month-change-btn"
+            onClick={() => setSelectedMont((m) => subMonths(m, 1))}
+          >
+            &lt;
+          </button>
+          <button
+            className="month-change-btn"
+            onClick={() => setSelectedMont((m) => addMonths(m, 1))}
+          >
+            &gt;
+          </button>
         </div>
-        <span className="month-title">June 2023</span>
+        <span className="month-title">
+          {formatDate(selectedMonth, { month: "long", year: "numeric" })}
+        </span>
       </div>
       <div className="days">
         {calendarDays.map((day, i) => (
