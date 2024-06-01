d63e61869698d8cf3a3c8c6dba99ebd4e5ff8379
towmie <andre00toz@gmail.com>
2024-06-01 17:47:06 +0300

    fix: modal closing animation

--- src/context/events.tsx
+++ src/context/events.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useEffect, useState } from "react";
 import { UnionOmit } from "../utils.js/types";
 import { EVENTS_COLOR } from "./useEvents";
 
@@ -26,7 +26,7 @@ export type EventProviderProps = {
 export const Context = createContext<EventsContext | null>(null);
 
 export function EventsProvider({ children }: EventProviderProps) {
-  const [events, setEvents] = useState<Event[]>([]);
+  const [events, setEvents] = useLocalStorage("events", []);
 
   function addEvent(event: UnionOmit<Event, "id">) {
     setEvents((e) => [...e, { ...event, id: crypto.randomUUID() }]);
@@ -47,4 +47,23 @@ export function EventsProvider({ children }: EventProviderProps) {
       {children}
     </Context.Provider>
   );
+}
+
+function useLocalStorage(key: string, initialvalue: Event[]) {
+  const [value, setValue] = useState<Event[]>(() => {
+    const jsonValue = localStorage.getItem(key);
+
+    if (jsonValue == null) return initialvalue;
+
+    return (JSON.parse(jsonValue) as Event[]).map((ev) => {
+      if (ev.date instanceof Date) return ev;
+      return { ...ev, date: new Date(ev.date) };
+    });
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue] as const;
 }
